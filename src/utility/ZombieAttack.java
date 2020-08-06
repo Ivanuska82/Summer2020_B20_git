4e364f78f52d3966f776f909a560c9a081b79ff7
Ivanuska82 <reynoldsivana82@gmail.com>
2020-08-05 20:31:06 -0400

    add ReverseAString.java

--- src/utility/ZombieAttack.java
+++ src/utility/ZombieAttack.java
@@ -2,6 +2,8 @@ package utility;
 import java.util.*;
 public class ZombieAttack {
     public static void main(String[] args) {
+
+
         Scanner input = new Scanner(System.in);
         int[] inhabitants = new int[8];
         for(int i=0; i<inhabitants.length; i++) {
